4a0d3c3b53825e9c7939258a6801bd4f1bc44e75
Sam <samuel.brb19@gmail.com>
2022-11-09 23:43:58 +0100

    adding styles

--- views/homepage.view.php
+++ views/homepage.view.php
@@ -4,10 +4,25 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./views/styles/homepage.css">
 	<title>MyBlog - Homepage</title>
 </head>
 <body>
 	<?php require_once("./views/partials/nav.view.php"); ?>
-	<h1>Welcome to my blog</h1>
+
+	<h1 id="title">MyBlog</h1>
+	<div id="header">
+
+		<div id="catchphrase">
+			<h3>Welcome to MyBlog where you can share any tips about video game / web development</h3>
+			<h3>Feel free to post any tutos about something you're proud of</h3>
+			<h3>Even the simplest things can help someone who's starting</h3>
+			<h4>You can also ask for help if you're stuck somewhere !</h4>
+		</div>
+		<div id="cta">
+			<a href="./controller/register.controller.php" id="register" class="btn-cta">Register to join the community</a>
+			<a href="./controller/login.controller.php" id="login"class="btn-cta">Login to your account</a>
+		</div>
+	</div>
 </body>
 </html>
--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -1,6 +1,27 @@
+<style>
+	nav{
+		padding: 2%;
+		display: flex;
+		justify-content: space-between;
+	}
+	a {
+		text-decoration: none;
+	}
+	#link{
+		display: flex;
+		gap: 15px;
+	}
+	#link > a:hover{
+		text-decoration: underline;
+	}
+</style>
 <nav>
-	<a href="/Projet5/index.php">Back To Home</a>
-	<a href="controller/login.controller.php">Login</a>
-	<?= isset($_SESSION["username"]) ? "" : '<a href="controller/register.controller.php">Register</a>' ?>
-	<p><?= isset($_SESSION["username"]) ? $_SESSION["username"] : "" ?></p>
+	<div id="logo">
+		<a href="/Projet5/index.php">MyBlog</a>
+	</div>
+	<div id="link">
+		<a href="controller/login.controller.php">Login</a>
+		<?= isset($_SESSION["username"]) ? "" : '<a href="controller/register.controller.php">Register</a>' ?>
+		<p><?= isset($_SESSION["username"]) ? $_SESSION["username"] : "" ?></p>
+	</div>
 </nav>
